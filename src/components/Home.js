9c56d49e4f4ff839b7552cad2d149ba1651a88a2
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-03-16 21:15:22 +0530

    Misc

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,12 @@ export const Home = () => {
                 <h1 className='text-center'>Add a Note</h1>
                 <form>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Note Title</label>
+                        <label for="exampleInputEmail1" className="form-label"><strong>Note Title</strong></label>
                         <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Description</label>
-                        <textarea type="text" rows={10} className="form-control" id="exampleInputPassword1"/>
+                        <label for="exampleInputPassword1" className="form-label"><strong>Description</strong></label>
+                        <textarea type="text" rows={8} className="form-control" id="exampleInputPassword1"/>
                     </div>
                     <button type="submit" className="btn btn-dark">Submit</button>
                 </form>
