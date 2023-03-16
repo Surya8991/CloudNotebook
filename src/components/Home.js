f386ab824af367be45a90c5a5faa7b2e1387dd8f
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-03-16 21:38:05 +0530

    Added favicons & home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import Notes from './Notes'
-  
+import Notes from './Notes';
+
 export const Home = () => {
+
     return (
-        <>
-            <div className='container my-3 '>
-                <h1 className='text-center'>Add a Note</h1>
-                <form>
-                    <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label"><strong>Note Title</strong></label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    </div>
-                    <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label"><strong>Description</strong></label>
-                        <textarea type="text" rows={8} className="form-control" id="exampleInputPassword1"/>
-                    </div>
-                    <button type="submit" className="btn btn-dark">Submit</button>
-                </form>
+        <div>
+            <div className="container my-3">
+            <h2 className='text-center'>Add a Note</h2>
+            <form className="my-3">
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label"><strong>Note Title</strong></label>
+                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
+                    <textarea type="text" rows={8} className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <button type="submit" className="btn btn-dark">Submit</button>
+            </form>
             </div>
             <div className='container my-3'>
-                <Notes />
+            <h2 className='text-center'>Your Notes</h2>
+            <Notes/>
             </div>
-        </>
+        </div>
     )
 }
