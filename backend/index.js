c46d206321308023362ace058f83b32beba57f38
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-03-16 12:07:26 +0530

    Added Db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
+const connectToMongo = require('./db');
 const express = require('express')
+
+connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./Routes/auth'))
+app.use('/api/notes', require('./Routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`iNoteBook Backend Listening on ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
